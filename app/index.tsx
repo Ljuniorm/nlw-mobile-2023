5e2b58a5f732e81d5ba51588b166ecf7028d528c
Luiz Junior <luiz.junior@blueticket.com.br>
2023-05-19 00:01:34 -0300

    Login with github; Routes added

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { ImageBackground, Text, View, TouchableOpacity } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { useRouter } from "expo-router";
 
-import blurBg from "./src/assets/luz.png";
-import Stripes from "./src/assets/stripes.svg";
-import NLWLogo from "./src/assets/nlwLogo.svg";
+import blurBg from "../src/assets/luz.png";
+import Stripes from "../src/assets/stripes.svg";
+import NLWLogo from "../src/assets/nlwLogo.svg";
 
 import {
   useFonts,
@@ -14,9 +16,52 @@ import {
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 import { styled } from "nativewind";
 
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { useEffect } from "react";
+import { api } from "../src/lib/api";
+
 const StyledStripes = styled(Stripes);
 
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/7b0a6b0e5179a4783265",
+};
+
 export default function App() {
+  const router = useRouter();
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: "7b0a6b0e5179a4783265",
+      scopes: ["identity"],
+      redirectUri: makeRedirectUri({
+        scheme: "nlwspacetime",
+      }),
+    },
+    discovery
+  );
+
+  async function handleGithubAuth(code: string) {
+    const resp = await api.post("/register", {
+      code,
+    });
+
+    const { token } = resp.data;
+    await SecureStore.setItemAsync("token", token);
+
+    router.push("/memories");
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { code } = response.params;
+
+      handleGithubAuth(code);
+    }
+  }, [response]);
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -49,6 +94,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => promptAsync()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar Lembraça
